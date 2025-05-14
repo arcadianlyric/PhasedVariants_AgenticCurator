58a351c584fc85c9cd6493ef58594906ba8d8e8c
Ying <yingcai1123@gmail.com>
2025-05-13 19:53:43 -0700

    init

--- vcf/vep_v2.py
+++ vcf/vep_v2.py
@@ -1,5 +1,5 @@
 '''
-python vep.v2.py
+python vep.v2.py --chrom 22 --start 1 --end 26466075 --vcf_file ../data/high.vcf.gz
 output:
 genes with HIGH impact in 2 copies
 
@@ -12,6 +12,18 @@ fixed missing gene name in vep.py
 import pysam
 import re
 from collections import defaultdict
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--chrom", type=str, required=False)
+parser.add_argument("--start", type=int, required=False)
+parser.add_argument("--end", type=int, required=False)
+parser.add_argument("--vcf_file", type=str, required=False)
+
+args = parser.parse_args()
+chrom = args.chrom
+start = args.start
+end = args.end
+vcf_file = args.vcf_file
 
 def create_transcript_to_gene_map(vep_vcf_file):
     """Create a dictionary mapping transcript IDs to gene names from VEP VCF output."""
@@ -181,7 +193,7 @@ def analyze_haplotypes(vep_vcf_file, chrom37, start, end):
     
     return hap1_results, hap2_results
 
-def group_gene_ps(hap1_results, hap2_results):
+def group_gene_ps(hap1_results, hap2_results, chrom, start, end):
     """Group haplotypes by gene and PS, focusing on IMPACT=HIGH variants."""
     hap1_dict = defaultdict(lambda: defaultdict(list))
     hap2_dict = defaultdict(lambda: defaultdict(list))
@@ -213,34 +225,40 @@ def group_gene_ps(hap1_results, hap2_results):
         hap2_dict[gene][ps].append((vep, pos, impact, transcript_id, genotype, allele))
 
     print(f'LoF (IMPACT=HIGH)')
-    for gene, ps_dict in hap1_dict.items():
-        if gene == 'N/A':
-            continue
-        for ps, variants1 in ps_dict.items():
-            variants2 = hap2_dict[gene][ps]
-            if ps == 'hom_alt':
-                if any(vep[2] == 'HIGH' for vep in variants1):
-                    v_lof = [vep for vep in variants1 if vep[2] == 'HIGH']
-                    print(f'hom_alt Gene: {gene}, PS: {ps}, Variants: {v_lof}')
-            else:
-                if any(vep[2] == 'HIGH' for vep in variants1) and any(vep[2] == 'HIGH' for vep in variants2):
-                    v1 = [vep for vep in variants1 if vep[2] == 'HIGH']
-                    v2 = [vep for vep in variants2 if vep[2] == 'HIGH']
-                    print(f'het_alt Gene: {gene}, PS: {ps}, Variants: {v1}, {v2}')
+    gene_names = []
+    with open(f'chr{chrom}_{start}_{end}_vep_results.txt', 'w') as f:
+        for gene, ps_dict in hap1_dict.items():
+            if gene == 'N/A':
+                continue
+            for ps, variants1 in ps_dict.items():
+                variants2 = hap2_dict[gene][ps]
+                if ps == 'hom_alt':
+                    if any(vep[2] == 'HIGH' for vep in variants1):
+                        v_lof = [vep for vep in variants1 if vep[2] == 'HIGH']
+                        f.write(f'hom_alt Gene: {gene}, PS: {ps}, Variants: {v_lof}\n')
+                        gene_names.append(gene)
+                else:
+                    if any(vep[2] == 'HIGH' for vep in variants1) and any(vep[2] == 'HIGH' for vep in variants2):
+                        v1 = [vep for vep in variants1 if vep[2] == 'HIGH']
+                        v2 = [vep for vep in variants2 if vep[2] == 'HIGH']
+                        f.write(f'het_alt Gene: {gene}, PS: {ps}, Variants: {v1}, {v2}\n')
+                        gene_names.append(gene)
 
-    print(f'Number of genes in hap1_dict: {len(hap1_dict)}')
-    for gene, ps_dict in hap1_dict.items():
-        print(f'{gene}: {ps_dict}')
-        print('')
+    # print(f'Number of genes in hap1_dict: {len(hap1_dict)}')
+    # for gene, ps_dict in hap1_dict.items():
+    #     print(f'{gene}: {ps_dict}')
+    #     print('')
 
-    print(f'Number of genes in hap2_dict: {len(hap2_dict)}')
-    for gene, ps_dict in hap2_dict.items():
-        print(f'{gene}: {ps_dict}')
-        print('')
+    # print(f'Number of genes in hap2_dict: {len(hap2_dict)}')
+    # for gene, ps_dict in hap2_dict.items():
+    #     print(f'{gene}: {ps_dict}')
+    #     print('')
 
-    return hap1_dict, hap2_dict
+    print(gene_names)
+    return hap1_dict, hap2_dict, gene_names
 
 if __name__ == "__main__":
-    chrom, start, end = "22", 1, 26466075
-    hap1_results, hap2_results = analyze_haplotypes('../data/high.vcf.gz', chrom, start, end)
-    group_gene_ps(hap1_results, hap2_results)
+    # chrom, start, end = "22", 1, 26466075
+    # vcf_file = '../data/high.vcf.gz'
+    hap1_results, hap2_results = analyze_haplotypes(vcf_file, chrom, start, end)
+    hap1_dict, hap2_dict, gene_names = group_gene_ps(hap1_results, hap2_results, chrom, start, end)
